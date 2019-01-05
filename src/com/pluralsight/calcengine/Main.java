45049a259cac8c06c11d0c815fbf52a7c6d71d0d
Hanh Pham <hanhtp2001@gmail.com>
2019-01-05 15:02:27 -0800

    testing git comments

--- src/com/pluralsight/calcengine/Main.java
+++ src/com/pluralsight/calcengine/Main.java
@@ -8,7 +8,6 @@ public class Main {
         equations[1]=create(25.0d,92.0d,'a');
         equations[2]=create(225.0d,17.0d,'s');
         equations[3]=create(11.0d,3.0d,'m');
-//adding comments
         for(MathEquation equation:equations) {
             equation.execute();
 
